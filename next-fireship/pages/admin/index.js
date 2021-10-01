c31112cee75a634c647147a125956a603b0499ef
Luvdipsingh Dadiyala <ldadiyala@gmail.com>
2021-10-01 16:14:47 +0530

    react toast added

--- next-fireship/pages/admin/index.js
+++ next-fireship/pages/admin/index.js
@@ -1,10 +1,13 @@
 import Loader from "../../components/Loader";
+import toast, { Toaster } from "react-hot-toast";
 
+const notify = () => toast.success("Successfully Created");
 export default function AdminPostPage() {
   return (
     <main>
       <h1>Admin ka pages</h1>
-      <Loader show={1} />
+      <button onClick={notify}>Make me a toast</button>
+      {/* <Loader show={1} /> */}
     </main>
   );
 }
